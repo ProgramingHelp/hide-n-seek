a7f2ec7ed2ec5c4672abfce38b1786d5cefa3953
BuildTools <unconfigured@null.spigotmc.org>
2024-06-15 00:41:51 +0300

    - added info to the manifest - made check for id

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from pywebio import *
 from pywebio.input import *
 from pywebio.output import *
+import json
+
+file = open("manifest.json")
+json_file = json.load(file)
+
 
 def main():
     id = input("enter id's of the players")
     list = id.split()
+    for x in range(len(list)):
+        if list[x] in json_file:
+            print(f"{list[x]} is in the manifest")
     put_text(list)
     print(list)
 
